4906941f8c21ee7f6e1ff91636b7113682d1faa6
2gbeh <tugbeh.osaretin@gmail.com>
2024-04-13 18:47:06 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# marina-cms
-Reusable Admin Dashboard (Next.js 14 + Shadcn UI)
+# Marina CMS
+
+Reusable Admin Dashboard - Next.js 14 + Shadcn/ui
+
+[![Next.js](https://img.shields.io/badge/Next.js-14.x-111111.svg)](https://nextjs.org/docs)
+[![TypeScript](https://img.shields.io/badge/TypeScript-5.x-blue.svg)](https://www.typescriptlang.org/docs/)
+[![PostgreSQL](https://img.shields.io/badge/PostgreSQL-16.x-316192.svg)](https://www.postgresql.org/docs/16/index.html)
+[![Supbase](https://img.shields.io/badge/Supbase-0.24-3ECF8E.svg)](https://supabase.com/docs/guides/getting-started/quickstarts/nextjs)
+
+[![Vercel](https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white)](https://marina-cms.vercel.app)
+
+## Installation
+
+```
+$ git clone https://github.com/2gbeh/marina-cms.git
+
+$ cd marina-cms
+
+$ npm install
+
+$ npm run dev
+```
+
+## Usage
+
+> Local:   http://localhost:3000/
+
+## Documentation
+
+![Screenshot](./public/social-preview.png)
---
+++ src/lib/supabase/supabase.config.ts
@@ -0,0 +1,45 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+// import { createClient } from "@/utils/supabase/server";
+
+// export default async function Notes() {
+//   const supabase = createClient();
+//   const { data: notes } = await supabase.from("notes").select();
+
+//   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+// }
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+}
